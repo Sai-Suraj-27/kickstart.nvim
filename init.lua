e90da1a6e948bd15e2fe8acc132408c26659157f
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-08-14 22:54:40 +0530

    Updated neovim config.

--- init.lua
+++ init.lua
@@ -181,9 +181,8 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  To check the current status of your plugins, run
 --    :Lazy
---
+
 --  You can press `?` in this menu for help. Use `:q` to close the window
---
 --  To update plugins you can run
 --    :Lazy update
 --
